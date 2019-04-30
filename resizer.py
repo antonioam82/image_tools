9d9db3e2a812ecca03160797d1e3a7bfaf874166
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-04-30 23:45:13 +0200

    Update resizer.py

--- resizer.py
+++ resizer.py
@@ -18,20 +18,23 @@ def resuze(fn,tama):
     w,h=im.size
     im.thumbnail((tama,int(tama*h/w)),Image.ANTIALIAS)
     im.save(file+"_resized"+ext,"PNG")
-    #im.thumbnail((40,int(40.*h/w)),Image.ANTIALIAS)#150
-    #im.save(file+"_thumbnail"+ext,'PNG')
 
 while True:
-    print("\n______________________________")
+    print("")
+    print("_______________________________")
     print("|                             |")
     print("|      --IMAGE RESIZER--      |")
     print("|_____________________________|\n")
     
     imagen=input("Nombre imagen: ")
-    
-    tama=OKI(input("Nuevo tamaño: "))
-    resuze(imagen,tama)
+    if imagen in os.listdir():
+        tama=OKI(input("Nuevo tamaño: "))
+        resuze(imagen,tama)
+        print("\nACCIÓN COMPLETADA CON ÉXITO")
+    else:
+        print("\nNo se encontró el archivo",imagen)
     conti=ns(input("\n¿Desea continuar?: "))
     if conti=="n":
         break
     subprocess.call(["cmd.exe","/C","cls"])
+    
