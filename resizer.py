5c3b1d49ffd8e484a715fd0820a0cc0d06dfeb4e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-04 23:47:55 +0200

    Update resizer.py

--- resizer.py
+++ resizer.py
@@ -2,8 +2,19 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
 import os.path
-#from VALID import ns, OKI
 import subprocess
+
+def OKI(n):
+    try:
+        n=int(n)
+    except:
+        n=OKI(input("Caracter no valido: "))
+    return n
+
+def ns(c):
+    while c!=("s") and c!=("n"):
+        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
+    return(c)
 
 while True:
     nueva_ruta=input("Introduzca ruta: ")
@@ -12,25 +23,19 @@ while True:
         break
     else:
         print("RUTA NO VALIDA")
-        
-def ns(c):
-    while c!=("s") and c!=("n"):
-        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
-    return(c)
- 
-def OKI(n):
-    try:
-        n=int(n)
-    except:
-        n=OKI(input("Caracter no valido: "))
-    return n
     
-def resuze(fn,tama):
+
+def resize(fn,tama):
     file, ext = os.path.splitext(fn)
     im = Image.open(fn)
     w,h=im.size
-    im.thumbnail((tama,int(tama*h/w)),Image.ANTIALIAS)
-    im.save(file+"_resized"+ext,"PNG")
+    if tama < w:
+        im.thumbnail((tama,int(tama*h/w)),Image.ANTIALIAS)
+        im.save(file+"_resized"+ext,"PNG")
+        print("\nACCIÓN COMPLETADA CON ÉXITO")
+    else:
+        print("El tamaño solicitado es igual o menor al original",im.size)
+
 
 while True:
     print("")
@@ -42,12 +47,13 @@ while True:
     imagen=input("Nombre imagen: ")
     if imagen in os.listdir():
         tama=OKI(input("Nuevo tamaño: "))
-        resuze(imagen,tama)
-        print("\nACCIÓN COMPLETADA CON ÉXITO")
+        resize(imagen,tama)
     else:
         print("\nNo se encontró el archivo",imagen)
     conti=ns(input("\n¿Desea continuar?: "))
     if conti=="n":
         break
     subprocess.call(["cmd.exe","/C","cls"])
+    
+
     
