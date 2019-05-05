0c9945c6fbcc0e598dbb1b89507557a46e94a6ba
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-05 11:46:47 +0200

    Update resizer.py

--- resizer.py
+++ resizer.py
@@ -29,23 +29,23 @@ def resize(fn):
     file, ext = os.path.splitext(fn)
     try:
         im = Image.open(fn)
-        tama=OKI(input("Nuevo tamaño: "))
-        w,h=im.size
+        tama = OKI(input("Nuevo tamaño: "))
+        w,h = im.size
         if tama < w:
             im.thumbnail((tama,int(tama*h/w)),Image.ANTIALIAS)
             im.save(file+"_resized"+ext,"PNG")
             print("\nACCIÓN COMPLETADA CON ÉXITO")
         else:
-            print("\nEl tamaño solicitado es igual o menor al original",im.size)
+            print("\nEl tamaño solicitado es igual o mayor al original",im.size)
     except:
-        print("\nEl archivo solicitado no es apto para la operación")
+        print("\nNo pudo realizarse la conversión de",fn)
 
 
 while True:
     print("")
     print("_______________________________")
     print("|                             |")
-    print("|      --IMAGE RESIZER--      |")
+    print("|   --CREATING THUMBNAILS--   |")
     print("|_____________________________|\n")
     
     imagen=input("Nombre imagen: ")
@@ -53,7 +53,7 @@ while True:
         resize(imagen)
     else:
         print("\nNo se encontró el archivo",imagen)
-    conti=ns(input("\n¿Desea continuar?: "))
+    conti = ns(input("\n¿Desea continuar?: "))
     if conti=="n":
         break
     subprocess.call(["cmd.exe","/C","cls"])
