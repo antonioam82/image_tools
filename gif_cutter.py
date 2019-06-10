9d9b3562d7f27905a7d7127ae9ca166c031e1270
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-10 23:36:26 +0200

    Update gif_cutter.py

--- gif_cutter.py
+++ gif_cutter.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 from PIL import Image, ImageSequence
 import os, subprocess
-from VALID import ns, OKI
 
 while True:
     nueva_ruta=input("Introduzca ruta: ")
@@ -12,6 +11,18 @@ while True:
         break
     else:
         print("RUTA NO VALIDA")
+
+def ns(c):
+    while c!=("s") and c!=("n"):
+        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
+    return(c)
+        
+def OKI(n):
+    try:
+        n=int(n)
+    except:
+        n=OKI(input("Caracter no valido: "))
+    return n
 
 def busca():
     archiv=input("Nombre del archivo: ")
