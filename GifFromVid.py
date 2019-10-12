e014925dc6796dc43ada3edf0b21a0395c4c1fa2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-12 17:38:12 +0200

    Update GifFromVid.py

--- GifFromVid.py
+++ GifFromVid.py
@@ -1,18 +1,27 @@
 from moviepy.editor import *
 import os
-from VALID import direc
+import subprocess
+from VALID import direc, ns
 
 direccion = direc()
 
-vid = input("Introducir video:" )
-start = input("Inicio: ")
-end = input("Final: ")
-inicio = (start).split(",")
-ended = (end).split(",")
-name = input("Nombre gif: ")
+while True:
+    vid = input("Introducir video:" )
+    start = input("Inicio: ")
+    end = input("Final: ")
+    inicio = (start).split(",")
+    ended = (end).split(",")
+    name = input("Nombre gif: ")
 
-
-clip = (VideoFileClip(vid)
-        .subclip((float(inicio[0]),float(inicio[1])),(float(ended[0]),float(ended[1]))))
-
-clip.write_gif(name)
+    try:
+        clip = (VideoFileClip(vid)
+            .subclip((float(inicio[0]),float(inicio[1])),
+            (float(ended[0]),float(ended[1]))))
+        clip.write_gif(name)
+    except:
+        print("Hubo un problema al realizar la operación")
+        
+    conti = ns(input("¿Desea continuar?: "))
+    if conti == "n":
+        break
+    subprocess.call(["cmd.exe","/C","cls"])
