36d526fda1064af19dbcc7ca7f3414c1ff9ee8de
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-03 21:10:15 +0100

    Update gif_cutter2.py

--- gif_cutter2.py
+++ gif_cutter2.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 from PIL import Image, ImageSequence
 import os, subprocess
-import shutil
 from VALID import ns, OKI
 
 while True:
@@ -20,16 +19,7 @@ def busca():
         archiv=archiv+".gif"
     return archiv
 
-def carpeta_nueva(m,n):
-    if not os.path.exists(n):
-        os.makedirs(n)
-    shutil.move(m,n)
-
-#formatos = ["P", "L", "RGB", "RGBA", "LA"]
-
 while True:
-
-    carpeta_destino="frames"
 
     print("")
     print("_____________________________")
@@ -37,35 +27,27 @@ while True:
     print("|       --GIF CUTTER--      |")
     print("|___________________________|")
     print("")
-
-    #giff=input("Nombre del archivo: ")
-    gif_name=busca()
-
-    while not gif_name in os.listdir():
-        print("NO SE ENCONTRÓ EL ARCHIVO",gif_name)
+    
+    while True:
         gif_name=busca()
-        
-    name,ext=os.path.splitext(gif_name)
-        
-    try:
-        im=Image.open(gif_name)
-        print("\nDimensiones: ",im.size[0],"x",im.size[1])
-        print("")
-    except:
-        print("\nNo se pudo abrir el archivo",gif_name)
-        continue
-
-    #while True:
-        #form=input("Fromato: ")
-        #if form in formatos:
-            #continue
-        #else:
-            #break
+        while not gif_name in os.listdir():
+            print("NO SE ENCONTRÓ EL ARCHIVO",gif_name)
+            gif_name=busca()
+        name,ext=os.path.splitext(gif_name)
+        titl=" frame"
+        try:
+            im=Image.open(gif_name)
+            print("\nDimensiones: ",im.size[0],"x",im.size[1])
+            print("")
+            break
+        except:
+            print("\nNo se pudo abrir el archivo",gif_name)
+            os.remove(gif_name)
 
     corte=ns(input("¿Desea realizar cortes sobre los frames?: "))
 
     if corte=="s":
-        carpeta_destino="cropped"
+        titl=" cropped"
         dato_iz=OKI(input("Introduce dato izquierdo: "))
         dato_sup=OKI(input("Introduce dato superior: "))
         dato_der=OKI(input("Introduce dato derecho: "))
@@ -73,23 +55,21 @@ while True:
     
         box=(dato_iz, dato_sup, dato_der, dato_inf)
     count=1
-    
+
     for frame in ImageSequence.Iterator(im):
         try:
             if corte=="s":
                 n_imagen=im.crop(box)
             else:
-                n_imagen=im#convert(form)
-            nom_imagen=name+str(count)+'.png'
+                n_imagen=im
+            nom_imagen=name+titl+str(count)+'.png'
             n_imagen.save(nom_imagen)
             print("Extraído frame: ",nom_imagen)
-            carpeta_nueva(nom_imagen,carpeta_destino)
             count += 1
         except:
             print("La operación no pudo completarse con éxito")
-            os.remove(name+str(count)+'.png')
+            os.remove(nom_imagen)
             break
-    print("\nLos archivos han sido guardados en la carpeta", carpeta_destino)
         
     
     print("")
