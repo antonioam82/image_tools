922efb87e0353ecfd3371676b4c83d35c6d9fc5c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-01 20:46:34 +0100

    Update gif_cutter3.py

--- gif_cutter3.py
+++ gif_cutter3.py
@@ -1,11 +1,23 @@
 import Pmw
 import PIL
+from tkinter import filedialog
 
 ventana = Pmw.initialise(fontScheme = 'pmw1')
-ventana.title("GIF CUTT3R")
+ventana.title("GIF CUTTER")
+
+def texto():
+    display.appendtext("HOla")
 
 def clear():
     display.clear()
+
+def busca():
+    archivo_selec=filedialog.askopenfilename(initialdir = "/",
+    title = "Seleccione archivo",filetypes = (("gif","*.*"),
+    ("webp","*.*")))
+    if archivo_selec!="":
+        display.appendtext("\nDir: "+archivo_selec)
+    
 
 def texto_inicio():
     display.appendtext("Pulse \'BUSCAR\' para escoger archivo.")
@@ -13,18 +25,19 @@ def texto_inicio():
 display = Pmw.ScrolledText(ventana, hscrollmode='none',
                       vscrollmode='dynamic', hull_relief='sunken',#vscrollmode=dynamic
                       hull_background='gray20', hull_borderwidth=10,
-                      text_background='honeydew4', text_width=50, #ancho pantalla
-                      text_foreground='black', text_height=15, #alto pantalla
+                      text_background='honeydew4', text_width=55, #ancho pantalla
+                      text_foreground='black', text_height=17, #alto pantalla
           text_padx=10, text_pady=10, text_relief='groove',
                       text_font=('Fixedsys', 10))
 display.pack(padx=0,pady=0)
 
 buttons = Pmw.ButtonBox(ventana)
+
 buttons.pack(fill='both', expand=1, padx=1, pady=1)
 
-buttons.add('LIMPIAR',bg='light blue',command=clear,width=15)
+buttons.add('LIMPIAR',bg='light blue',command=clear,width=17)
 buttons.add('CORTAR',bg='light blue')
-buttons.add('BUSCAR',bg='light blue')
+buttons.add('BUSCAR',bg='light blue',command=busca)
 buttons.alignbuttons()
 
 texto_inicio()
