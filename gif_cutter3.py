91865532aa3237f3e9e6c26b977dc314ce16fb1e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-02 12:53:42 +0100

    Update gif_cutter3.py

--- gif_cutter3.py
+++ gif_cutter3.py
@@ -2,25 +2,43 @@ import Pmw
 import time
 from PIL import Image, ImageSequence
 from tkinter import filedialog
+import threading
+import os
 
 ventana = Pmw.initialise(fontScheme = 'pmw1')
 ventana.title("GIF CUTTER")
 archivo_selec = ""
+im = ""
 
 def clear():
     display.clear()
+    texto_inicio()
 
-#def corta():
-    #try:
+def iniciar_extract():
+    t=threading.Thread(target=corta)
+    t.start()
+
+def corta():
+    count=1
+    archivo=(((archivo_selec).split("/"))[-1])
+    try:
+        name,ex = os.path.splitext(archivo)
+        for frame in ImageSequence.Iterator(im):
+            nom_imagen=name+" "+str(count)+'.png'
+            im.save(nom_imagen)
+            display.appendtext("\nExtraido frame: "+nom_imagen)
+            count+=1
+    except:
+        display.appendtext("\nHUBO UN PROBLEMA AL REALIZAR LA OPERACIÓN")
         
 def busca():
     global archivo_selec
+    global im
     archivo_selec=filedialog.askopenfilename(initialdir = "/",
     title = "Seleccione archivo",filetypes = (("gif","*.*"),
     ("webp","*.*")))
     if archivo_selec!="":
         try:
-            archivo=(((archivo_selec).split("/"))[-1])
             im=Image.open(archivo_selec)
             time.sleep(1)
             display.appendtext("Dir: "+archivo_selec+"\n")
@@ -46,10 +64,11 @@ buttons = Pmw.ButtonBox(ventana)
 buttons.pack(fill='both', expand=1, padx=1, pady=1)
 
 buttons.add('LIMPIAR',bg='light blue',command=clear,width=17)
-buttons.add('CORTAR',bg='light blue')
+buttons.add('EXTRAER',bg='light blue',command=iniciar_extrac)
 buttons.add('BUSCAR',bg='light blue',command=busca)
 buttons.alignbuttons()
 
 texto_inicio()
 
 ventana.mainloop()
+
