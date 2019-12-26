29a282ee01d5addeebf15bf6c3e5a1382e2cf04a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-26 17:29:47 +0100

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,68 +1,19 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk, ImageSequence
 from tkinter import filedialog
 from tkinter.filedialog import askopenfilename, askdirectory
-from tkinter import messagebox
-from PIL import Image, ImageTk, ImageSequence
+import tkinter.scrolledtext as scrolledtext
+import threading
 import os
 
+def verify():
+    global ver
+    ver = True
+    display.insert(END,"RECORTE\n")
 
-# Variables globales
-_start = (0,0)#None    
-_end = (0,0)#None
-_img = None
-_photo = None
-_img_path = None
-_funcids = {}
-
-def _enable_croping():
-    global canvas
-    _funcids["<Button-1>"] = canvas.bind("<Button-1>", _on_click, '+')
-    _funcids["<B1-Motion>"] = canvas.bind("<B1-Motion>", _on_drag, '+')
-    _funcids["<ButtonRelease-1>"] = canvas.bind("<ButtonRelease-1>", _on_drop, '+')
-    print("enabled")
-
-def _disable_croping():
-    global canvas
-    global crop_btn
-    for event, funcid in _funcids.items():
-        canvas.unbind(event, funcid)
+def delete_rectangle(event):
+    canvas.delete("rectangle")
     crop_btn.config(state="disabled")
-
-def direc():
-    directorio=filedialog.askdirectory()
-    if directorio!="":
-        os.chdir(directorio)
-
-def _open_image():
-    global _img
-    global _photo
-    global _img_path
-    global canvas
-    global nombre
-    
-
-    file = askopenfilename(parent=root, initialdir="M:/",title='Choose an image.')
-    nombre=(((file).split("/"))[-1])
-    if not file:
-        return None
-
-    canvas.delete("all")
-    try:
-        _img = Image.open(file)
-        print(_img.size)
-    except OSError:
-        _img = None
-        _photo = None
-        _img_path = None
-        _disable_croping()
-    else:
-        _photo = ImageTk.PhotoImage(_img)
-        _img_path = file
-        canvas.create_image(0, 0, image=_photo, anchor="nw", tags="image")
-        _enable_croping()
-    finally:
-        canvas.config(scrollregion=canvas.bbox(tk.ALL))
-
 
 def _on_click(event):
     global _start
@@ -71,66 +22,7 @@ def _on_click(event):
     
     _start = (canvas.canvasx(event.x), canvas.canvasy(event.y))
     _end = None
-
-def _on_drop(event):
-    global _start
-    global _end
-    global _img
-    global crop_btn
-
-    if _end is None:
-        crop_btn.config(state="disabled")
-
-    else:
-
-        # Acotar límites de seleción a la imagen
-        img_x, img_y = _img.size
-
-        x0, y0 = _start
-        x0 = img_x if x0 > img_x else 0 if x0 < 0 else x0
-        y0 = img_y if y0 > img_y else 0 if y0 < 0 else y0 
-        _start = (x0, y0)
-
-        x1, y1 = _end
-        x1 = img_x if x1 > img_x else 0 if x1 < 0 else x1
-        y1 = img_y if y1 > img_y else 0 if y1 < 0 else y1       
-        _end = (x1, y1)
-
-        # Normalizado para obtener vertice superior izquierdo e inferior derecho
-        if x0 > x1:
-            if y0 < y1: # _start es el vértice superior derecho
-                _start = (x1, y0)
-                _end = (x0, y1)
-            else:       # _start es el vértice inferior derecho
-                _start, _end = _end, _start
-        else:
-            if y0 > y1:  # _start es el vértice inferior izquierdo
-                _start = (x0, y1)
-                _end = (x1, y0)
-
-        crop_btn.config(state="normal")
-
-    # Redibujar rectágulo
-    _draw_rectangle()
-
-def iniciar_extract():
-    if archivo_selec!="" and _img!="":
-        t=threading.Thread(target=_crop_image)
-        t.start()
-    else:
-        display.appendtext("\nSELECCIONE UN ARCHIVO\n")
-
-
-def _on_drag(event):
-    global _start
-    global _end
-    global canvas
-
-    x0, y0 = _start
-    ex, ey = canvas.canvasx(event.x), canvas.canvasy(event.y)
-    _end = (ex, ey)
-    _draw_rectangle()
-
+    crop_btn.config(state="normal")
 
 def _draw_rectangle():
     global canvas
@@ -144,62 +36,176 @@ def _draw_rectangle():
 
     x0, y0 = _start
     x1, y1 = _end
+   
 
     canvas.create_rectangle(x0, y0, x1, y1, fill="#18c194",
                             width=1, stipple="gray50", tags='rectangle'
                             )
-
-def _crop_image():
-    global _img
+    
+def _on_drag(event):
     global _start
     global _end
-    crop_btn.config(state="disabled")
+    global canvas
+
+    x0, y0 = _start
+    ex, ey = canvas.canvasx(event.x), canvas.canvasy(event.y)
+    _end = (ex, ey)
+    _draw_rectangle()
+
+def clear():
+    global archivo_selec, im, ver
+    display.delete('1.0',END)
+    texto_inicio()
+    archivo_selec=""
+    im=""
+    ver = ""
+
+def recorte():
+    global _start, _end, canvas
+    root = Tk()
+    canv = Canvas(root, width=500, height=375, bg='white')
+    canv.grid(row=0, column=0)
+    img = ImageTk.PhotoImage(archivo_selec)
+    canv.create_image(0, 0, anchor=NW, image=img)
+    canv.pack(side="left")
+    
+    root.mainloop()
+
+def direc():
+    directorio=filedialog.askdirectory()
+    if directorio!="":
+        os.chdir(directorio)
+        display.insert(END,f"Dir: {os.getcwd()}"+"\n")
+
+def iniciar_extract():
+    if archivo_selec!="" and im!="":
+        t=threading.Thread(target=corta)
+        t.start()
+    else:
+        display.insert(END,"\nSELECCIONE UN ARCHIVO\n")
+
+def _on_drop(event):
+    global _start
+    global _end
+    global im
+
+    # Acotar límites de seleción a la imagen
+    img_x, img_y = im.size
+
+    x0, y0 = _start
+    x0 = img_x if x0 > img_x else 0 if x0 < 0 else x0
+    y0 = img_y if y0 > img_y else 0 if y0 < 0 else y0 
+    _start = (x0, y0)
+
+    x1, y1 = _end
+    x1 = img_x if x1 > img_x else 0 if x1 < 0 else x1
+    y1 = img_y if y1 > img_y else 0 if y1 < 0 else y1       
+    _end = (x1, y1)
+
+        # Normalizado para obtener vertice superior izquierdo e inferior derecho
+    if x0 > x1:
+        if y0 < y1: # _start es el vértice superior derecho
+            _start = (x1, y0)
+            _end = (x0, y1)
+        else:       # _start es el vértice inferior derecho
+            _start, _end = _end, _start
+    else:
+        if y0 > y1:  # _start es el vértice inferior izquierdo
+            _start = (x0, y1)
+            _end = (x1, y0)
+
+    # Redibujar rectágulo
+    _draw_rectangle()
+
+def recorte():
+    global _start, _end, canvas, crop_btn
+    if archivo_selec!="":
+        top = Toplevel()
+        canvas = Canvas(top,width=_end[0],height=_end[1],background='black')
+        canvas.pack(padx=0,pady=0)
+        archi = ImageTk.PhotoImage(Image.open(archivo_selec))
+        canvas.create_image(0,0,image=archi,anchor=NW)
+        canvas.bind('<Button-1>',_on_click)
+        canvas.bind("<B1-Motion>", _on_drag)
+        canvas.bind('<Button-3>',delete_rectangle)
+        canvas.bind("<ButtonRelease-1>", _on_drop, '+')
+        crop_btn = Button(top, text="Recortar imagen", state="disabled", bg="light green",command=verify)
+        crop_btn.pack(side="bottom",expand=1, fill=X)
+        top.mainloop()
+    else:
+        display.insert(END,"PULSE \'BUSCAR\' PARA SELECCIONAR UN ARCHIVO\n")
+
+def corta():
+    display.delete('1.0',END)
+    display.insert(END,"\nPROCESO EN CURSO\n")
     count=1
-    name,ex = os.path.splitext(nombre)
-    for frame in ImageSequence.Iterator(_img):
-        nom_imagen=name+" "+str(count)+'.png'
-        c_im=_img.crop(_start+_end)
-        c_im.save(nom_imagen)
-        count+=1
-    messagebox.showinfo("TAREA COMPLETADA","Proceso finalizado con éxito")
-    crop_btn.config(state="normal")
-    #cropped = _img.crop(_start + _end)
-    #cropped.show()
-    #cropped.save("RECORTE.png")
-
-root = tk.Tk()
-root.title("Recortar imagen")
-
-# Frame contenedor con canvas y barras de desplazamiento
-frame = tk.Frame(root, bd=2, relief=tk.SUNKEN)
-xscrollbar = tk.Scrollbar(frame, orient=tk.HORIZONTAL)
-yscrollbar = tk.Scrollbar(frame)
-canvas = tk.Canvas(frame, bd=0,
-                   xscrollcommand=xscrollbar.set, yscrollcommand=yscrollbar.set,width=480
-                   ,height=258
-                   )
-open_btn = tk.Button(root, text="Abrir imagen", command=_open_image)
-crop_btn = tk.Button(root, text="Recortar imagen", state="disabled", command=_crop_image)
-dir_btn = tk.Button(root, text="Carpeta", state="normal", command=direc)
-
-
-# Estructurando el widget
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-frame.grid_rowconfigure(0, weight=1)
-frame.grid_columnconfigure(0, weight=1)
-frame.grid(row=0, column=0, columnspan=2, sticky="nsew")
-xscrollbar.grid(row=1, column=0, columnspan=2, sticky="ew")
-yscrollbar.grid(row=0, column=1, sticky="ns")
-canvas.grid(row=0, column=0, sticky="nsew")
-open_btn.grid(row=2, column=0, sticky="ew")
-crop_btn.grid(row=2, column=1, sticky="nsew")
-dir_btn.grid(row=2,column=2,sticky="ew")
-
-xscrollbar.config(command=canvas.xview)
-yscrollbar.config(command=canvas.yview)
+    archivo=(((archivo_selec).split("/"))[-1])
+    try:
+        name,ex = os.path.splitext(archivo)
+        for frame in ImageSequence.Iterator(im):
+            nom_imagen=name+" "+str(count)+'.png'
+            c_im=im.crop(_start+_end)
+            c_im.save(nom_imagen)
+            #display.insert(END,"\nExtraido frame: "+nom_imagen)
+            count+=1
+        display.insert(END,"\n\nPROCESO FINALIZADO :D\n")
+    except:
+        display.insert(END,"\nHUBO UN PROBLEMA AL REALIZAR LA OPERACIÓN")
+        
+def busca():
+    global archivo_selec
+    global im, size, archivo
+    global _end
+    archivo_selec = askopenfilename(parent=ventana, initialdir="M:/",title='Elegir archivo.')
+    archivo=(((archivo_selec).split("/"))[-1])
+    if archivo_selec!="":
+        try:
+            im=Image.open(archivo_selec)
+            size=(im.size)
+            _end=size
+            display.insert(END,"Archivo seleccionado: "+(((archivo_selec).split("/"))[-1])+"\n")
+        except:
+            archivo_selec = ""
+            display.insert(END,"NO SE PUDO ABRIR EL ARCHIVO\n")
+    
+def texto_inicio():
+    display.insert(END,"_____________________________\n")
+    display.insert(END,"|                           |\n")
+    display.insert(END,"|       --GIF CUTTER--      |\n")
+    display.insert(END,"|___________________________|\n")
+    display.insert(END,"\n")
+    display.insert(END,"Pulse \'BUSCAR\' para escoger archivo.\nPulse \'CARPETA\' para escoger carpeta de destino.\n\n")
 
 
-# Inicio del mainloop de la app
-root.mainloop()
+ventana = Tk()
+ventana.title("GIF CUTTER")
+_start = (0,0)
+_end=""
+size=""
+archivo_selec = ""
+im = ""
+ver = False
+canvas = ""
+
+display = scrolledtext.ScrolledText(ventana,borderwidth=10,
+                      background='darkblue', width=65,
+                      foreground='light blue', height=22,
+                      padx=10, pady=10,#relief='groove'
+                      font=('Fixedsys', 10))
+display.pack(padx=0,pady=0)
+
+nframe = Frame()
+nframe.pack()
+
+Button(nframe,text='LIMPIAR',bg='light blue',command=clear,width=15,height=2).pack(side=LEFT)
+Button(nframe,text='CARPETA',bg='light blue',width=15,height=2,command=direc).pack(side=LEFT)
+Button(nframe,text='EXTRAER',bg='light blue',width=15,height=2,command=iniciar_extract).pack(side=LEFT)
+Button(nframe,text='BUSCAR',bg='light blue',width=15,height=2,command=busca).pack(side=LEFT)
+Button(nframe,text='RECORTAR',bg='light blue',width=15,height=2,command=recorte).pack(side=LEFT)
+
+texto_inicio()
+
+ventana.mainloop()
+
+
+
