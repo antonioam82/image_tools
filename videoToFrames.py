c2a8ae7e382089b79ca92b50818d43d103a0bd73
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-08-23 11:51:04 +0200

    Update videoToFrames.py

--- videoToFrames.py
+++ videoToFrames.py
@@ -3,6 +3,16 @@ from VALID import ns
 import cv2
 import os
 
+def ny():
+    while True:
+        q = input('Continue?(ny): ')
+        if q == 'n' or q == 'y':
+            return q
+            break
+        else:
+            print("Type y or n")
+
+
 os.chdir(r'C:\Users\Antonio\Documents\Nueva carpeta')
 
 while True:
@@ -14,32 +24,34 @@ while True:
     print("")
     
     root = input("Enter path to video: ")
+    if os.path.exists(root):
+        cam = cv2.VideoCapture(root)
+        #CREATE FRAME COUNTER.
+        currentframe = 0
 
-    cam = cv2.VideoCapture(root)
+        while(True):
+            #READ FROM FRAMES
+            ret,frame = cam.read()
 
-    #CREATE FRAME COUNTER.
-    currentframe = 0
-
-    while(True):
-        #READ FROM FRAMES
-        ret,frame = cam.read()
-
-        if ret:
-            #IF VIDEO IS STILL LETF, CONTINUE CREATING FRAMES.
-            name = 'frame'+str(currentframe)+'.jpg'
-            print('Creating...'+name)
+            if ret:
+                #IF VIDEO IS STILL LETF, CONTINUE CREATING FRAMES.
+                name = 'frame'+str(currentframe)+'.jpg'
+                print('Creating...'+name)
         
-            #WRITE THE EXTRACTED FRAME.
-            cv2.imwrite(name,frame)
+                #WRITE THE EXTRACTED FRAME.
+                cv2.imwrite(name,frame)
         
-            #INCREASE FRAME COUNTER.
-            currentframe += 1
-        else:
-            break
+                #INCREASE FRAME COUNTER.
+                currentframe += 1
+            else:
+                break
         
-    cam.release()
-    cv2.destroyAllWindows()
+        cam.release()
+        cv2.destroyAllWindows()
 
-    conti = ns(input("¿Continuar?(n/s): "))
+    else:
+        print('Wrong path')
+
+    conti = ny()
     if conti == "n":
         break
