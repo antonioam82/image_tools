30418e679a316e5c5740e4c9e068e230a278ee49
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-12-03 19:17:56 +0100

    Update glitchVideo_generator.py

--- glitchVideo_generator.py
+++ glitchVideo_generator.py
@@ -17,26 +17,29 @@ def define_name():
     return filename
 
 def convertToVideo(pathIn, pathOut, fps, time):
-    print("\nCREATING VIDEO...\n")
-    frame_array = []
-    #files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f)) and not 'glichtVid' in f]
-    files = [f for f in lista_frames if isfile(join(pathIn, f)) and not 'glichtVid' in f]
-    files.sort(key=lambda x: int((x.split(".")[0]).split(" ")[1]))#REORDENA FRAMES
-    for i in range(len(files)):
-        filename = pathIn+files[i]
-        print(filename)
-        img=cv2.imread(filename)
-        height, width, layers = img.shape
-        size = (width,height)
+    if len(lista_frames) > 0:
+        print("\nCREATING VIDEO...\n")
+        frame_array = []
+        #files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f)) and not 'glichtVid' in f]
+        files = [f for f in lista_frames if isfile(join(pathIn, f)) and not 'glichtVid' in f]
+        files.sort(key=lambda x: int((x.split(".")[0]).split(" ")[1]))#REORDENA FRAMES
+        for i in range(len(files)):
+            filename = pathIn+files[i]
+            print(filename)
+            img=cv2.imread(filename)
+            height, width, layers = img.shape
+            size = (width,height)
 
-        for k in range (time):
-            frame_array.append(img)
+            for k in range (time):
+                frame_array.append(img)
 
-    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
-    for i in range(len(frame_array)):
-        out.write(frame_array[i])
-    out.release()
-    print("TASK COMPLETED")
+        out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+        for i in range(len(frame_array)):
+            out.write(frame_array[i])
+        out.release()
+        print("TASK COMPLETED")
+    else:
+        print("\nNO SE GENERÓ VIDEO")
 
 def create_frames(d):
     global lista_frames
@@ -48,22 +51,25 @@ def create_frames(d):
     blu_rang = input("Rango azul: ").split(",")
     gre_rang = input("Rango verde: ").split(",")
     red_rang = input("Rango rojo: ").split(",")
+    
+    if fr_range > 0:
+        print("\nWRITTING "+str(fr_range)+" FRAMES...\n")
+        for i in range(0,fr_range):
+            img = np.zeros((height,width,3),np.uint8)#900,1600
 
-    print("\nWRITTING "+str(fr_range)+" FRAMES...\n")
-    for i in range(0,fr_range):
-        img = np.zeros((height,width,3),np.uint8)#900,1600
-
-        for x in range(height):#900
-            for y in range(width):#1600
-                img[x,y] = [random.randint(int(blu_rang[0]),int(blu_rang[1])),random.randint(int(gre_rang[0]),
-                            int(gre_rang[1])),random.randint(int(red_rang[0]),int(red_rang[1]))]#0,256
-        name = "ima "+str(i)+".png"
+            for x in range(height):#900
+                for y in range(width):#1600
+                    img[x,y] = [random.randint(int(blu_rang[0]),int(blu_rang[1])),random.randint(int(gre_rang[0]),
+                                int(gre_rang[1])),random.randint(int(red_rang[0]),int(red_rang[1]))]#0,256
+            name = "ima "+str(i)+".png"
         
-        cv2.imwrite(name,img)
-        print("DONE: ",name)
-        lista_frames.append(name)
-    print("TASK COMPLETED")
-    return frame_rate
+            cv2.imwrite(name,img)
+            print("DONE: ",name)
+            lista_frames.append(name)
+        print("TASK COMPLETED")
+        return frame_rate
+    else:
+        print("\n0 FRAMES GENERADOS")
 
 def validate_dir():
     while True:
@@ -83,7 +89,7 @@ while True:
     
     lista_frames=[]
 
-    directory = validate_dir() #input("Introduce directorio: ")#'C:/Users/Antonio/Documents/videos/imas'
+    directory = validate_dir()
     fps = create_frames(directory)
 
     pathIn = directory + '/'
@@ -91,10 +97,11 @@ while True:
     pathOut=pathIn + fileName
     time = 2
     convertToVideo(pathIn, pathOut, fps, time)
-    elim = ns(input("¿Eliminar frames generados?(n/s): "))
-    if elim == "s":
-        for i in lista_frames:
-            os.remove(i)
+    if len(lista_frames) > 0:
+        elim = ns(input("¿Eliminar frames generados?(n/s): "))
+        if elim == "s":
+            for i in lista_frames:
+                os.remove(i)
 
     conti = ns(input("¿Continuar?(n/s): "))
     if conti == "n":
