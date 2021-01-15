f53da8faee73d055ae9f2cda68757628ee679f05
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-15 10:48:48 +0100

    Update videoToFramesGUI.py

--- videoToFramesGUI.py
+++ videoToFramesGUI.py
@@ -14,6 +14,7 @@ class App:
         self.label = Label(self.ventana,text="NO FILE SELECTED",bg='gray45',fg='white')
         self.label.pack(side='top')
         self.file = ""
+        self.extract = True
         self.color = True
         self.executing = False
         self.ventana.title('VIDEO TO FRAMES')
@@ -28,12 +29,14 @@ class App:
         
         self.display.pack(padx=0,pady=0)
 
-        botones = Pmw.ButtonBox(self.ventana,hull_background="red")
+        botones = Pmw.ButtonBox(self.ventana,hull_background="gray20")
         botones.pack(fill='both',expand=1,padx=1,pady=1)
         self.btnMode = Button(self.ventana,text="CHANGE TO GRAY MODE",width=73,bg='gray30',fg='white',command=self.mode)
         self.btnMode.pack(side='bottom')
-        botones.add('SELECT VIDEO',width=35,bg='gray80',command=self.openFile)
-        botones.add('EXTRACT FRAMES',width=35,bg='gray80',command=self.initExtract)
+
+        botones.add('SELECT VIDEO',width=23,bg='gray80',command=self.openFile)
+        botones.add('EXTRACT FRAMES',width=23,bg='gray80',command=self.initExtract)
+        botones.add('STOP',width=23,bg='gray80',command=self.stop_pro)
 
         botones.alignbuttons()
 
@@ -55,7 +58,9 @@ class App:
         count = 1#0
         while(True):
             ret,frame = self.cam.read()
-            
+            if self.extract == False:
+                self.display.appendtext("\nSTOPPED\n")
+                break
             if ret:
                 name = self.name+" "+str(count)+'.jpg'
                 self.display.appendtext('Generated frame: {}\n'.format(name))
@@ -73,6 +78,9 @@ class App:
         self.cam.release()
         cv2.destroyAllWindows()
 
+    def stop_pro(self):
+        self.extract = False
+
     def mode(self):
         if self.color == True:
             self.color = False
@@ -85,12 +93,14 @@ class App:
 
     def initExtract(self):
         if self.file != "" and self.executing == False:
+            self.extract = True
             direct = filedialog.askdirectory()
             if direct != "":
                 self.executing = True
                 os.chdir(direct)
                 t = threading.Thread(target=self.extractFrames)
                 t.start()
+            
             
 if __name__=="__main__":
     App()
