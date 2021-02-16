d0b2a0c519c0b74b3f426bf41c61692856cadcae
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-02-16 16:00:48 +0100

    Update steganopy.py

--- steganopy.py
+++ steganopy.py
@@ -9,12 +9,15 @@ class app():
     def __init__(self):
         self.window = Tk()
         self.window.title("Image Steganography")
-        self.window.geometry("593x500")
+        self.window.geometry("593x520")
 
         self.entryDir = Entry(self.window,width=98)
         self.entryDir.place(x=0,y=0)
+        self.textEntry = sct.ScrolledText(self.window,width=70,height=15)
+        self.textEntry.place(x=5,y=28)
 
         self.window.mainloop()
 
 if __name__=="__main__":
     app()
+
