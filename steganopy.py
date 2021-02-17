5117ebd8d3b1558de2e164c3b6b3591be2bc25dd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-02-17 10:38:50 +0100

    Update steganopy.py

--- steganopy.py
+++ steganopy.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox, filedialog
 import tkinter.scrolledtext as sct
 import cv2
+import os
 import numpy as np
 
 class app():
@@ -11,7 +12,9 @@ class app():
         self.window.title("Image Steganography")
         self.window.geometry("593x520")
 
-        self.entryDir = Entry(self.window,width=98)
+        self.current_dir = StringVar()
+
+        self.entryDir = Entry(self.window,width=98,textvariable=self.current_dir)
         self.entryDir.place(x=0,y=0)
         self.textEntry = sct.ScrolledText(self.window,width=70,height=15)
         self.textEntry.place(x=5,y=28)
@@ -20,7 +23,14 @@ class app():
         self.btnClear = Button(self.window,text="CLEAR TEXT")
         self.btnClear.place(x=80,y=277)
 
+        self.show_dir()
+
         self.window.mainloop()
+
+    def show_dir(self):
+        dirr = os.getcwd()
+        self.current_dir.set(dirr)
+        
 
 if __name__=="__main__":
     app()
