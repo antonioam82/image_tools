8ea688dbf9d011550dc11cb079d4ed34408a2a41
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-23 19:33:24 +0200

    Create filtering.py

---
+++ filtering.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import cv2 as cv
+import os
+
+class app:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Filter")
+        self.root.geometry("900x400")
+
+        self.root.mainloop()
+
+
+if __name__=="__main__":
+    app()
