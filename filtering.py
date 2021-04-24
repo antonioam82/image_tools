7736ad1459dd00fc7b2ff4ab30d0c1726d6b74b1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-24 16:43:33 +0200

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -6,10 +6,16 @@ class app:
     def __init__(self):
         self.root = Tk()
         self.root.title("Filter")
-        self.root.geometry("900x400")
+        self.root.geometry("905x400")
+
+        self.currentDir = StringVar()
+        self.currentDir.set(os.getcwd())
+
+        Entry(self.root,textvariable=self.currentDir,width=150).place(x=0,y=0)
 
         self.root.mainloop()
 
 
 if __name__=="__main__":
     app()
+    
