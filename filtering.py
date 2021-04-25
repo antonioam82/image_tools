f5669adcf8a0aca1ce96f68090df9e5fb6c4e076
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-25 17:10:56 +0200

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -14,9 +14,11 @@ class app:
 
         Entry(self.root,textvariable=self.currentDir,width=150).place(x=0,y=0)
         Entry(self.root,textvariable=self.filename,font=('arial',23,'bold'),width=40).place(x=10,y=25)
-        Button(self.root,text="SEARCH",height=2,width=25).place(x=707,y=25)
+        Button(self.root,text="SEARCH",height=2,width=25).place(x=709,y=25)
+        Button(self.root,text="START",width=125).place(x=10,y=85)
 
         self.root.mainloop()
+
 
 if __name__=="__main__":
     app()
