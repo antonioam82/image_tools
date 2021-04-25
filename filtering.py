16b3ae4322a6797007517b5ca64b13f8f865086a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-25 17:24:22 +0200

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 import cv2 as cv
 import os
 
@@ -15,10 +16,17 @@ class app:
 
         Entry(self.root,textvariable=self.currentDir,width=150).place(x=0,y=0)
         Entry(self.root,textvariable=self.filename,font=('arial',23,'bold'),width=40).place(x=10,y=25)
-        Button(self.root,text="SEARCH",height=2,width=25,bg="light blue1").place(x=709,y=25)
+        Button(self.root,text="SEARCH",height=2,width=25,bg="light blue1",command=self.open_file).place(x=709,y=25)
         Button(self.root,text="START",width=125,bg="light green").place(x=10,y=85)
 
         self.root.mainloop()
+
+    def open_file(self):
+        fpath = filedialog.askopenfilename(initialdir = "/",
+                 title = "Select File",filetypes = (("png files","*.png"),
+                         ("jpg files","*.jpg"),("all files","*.*")))
+        if fpath:
+            self.filename.set(fpath.split("/")[-1])
 
 
 if __name__=="__main__":
