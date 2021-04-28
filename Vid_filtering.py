6fec11e3b0a4ec39ed481671d9dc9af082f2a665
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-28 16:37:37 +0200

    Update Vid_filtering.py

--- Vid_filtering.py
+++ Vid_filtering.py
@@ -25,12 +25,12 @@ class app:
         Entry(self.root,textvariable=self.filename,font=('arial',23,'bold'),width=40).place(x=10,y=25)
         Button(self.root,text="SEARCH",height=2,width=25,bg="light blue1",command=self.open_file).place(x=709,y=25)
         Button(self.root,text="START",width=97,height=2,bg="light green").place(x=8,y=77)
-        Button(self.root,text="SAVE",height=2,width=25,bg="light blue1").place(x=709,y=77)
+        Button(self.root,text="CONFIGURE",height=2,width=25,bg="light blue1").place(x=709,y=77)
         Label(self.root,text="FRAME RATE:",bg="lavender").place(x=709,y=150)
-        self.frLabel = Label(self.root,bg='black',width=14,fg="red")
+        self.frLabel = Label(self.root,bg='black',width=14,fg="light green")
         self.frLabel.place(x=790,y=150)
         Label(self.root,text="N FRAMES:",bg="lavender").place(x=721,y=190)
-        self.nframesLabel = Label(self.root,bg='black',width=14,fg="red")
+        self.nframesLabel = Label(self.root,bg='black',width=14,fg="light green")
         self.nframesLabel.place(x=790,y=190)
 
         self.root.mainloop()
@@ -39,6 +39,7 @@ class app:
         self.file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
                         filetypes=(("mp4 files","*.mp4"),("avi files","*.avi"),("all files","*.*")))
         if self.file != "":
+            self.filename.set((self.file).split("/")[-1])
             probe = ffmpeg.probe(self.file)
             self.video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
             self.fr = (self.video_streams[0]['avg_frame_rate'])
