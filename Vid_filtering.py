43704a26122b5e98d83e596ce1c9aea95aa3edbd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-28 16:18:30 +0200

    Update Vid_filtering.py

--- Vid_filtering.py
+++ Vid_filtering.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog, messagebox
 import cv2 as cv
+import ffmpeg
 import numpy as np
 import os
 
@@ -22,18 +23,28 @@ class app:
 
         Entry(self.root,textvariable=self.currentDir,width=158).place(x=0,y=0)
         Entry(self.root,textvariable=self.filename,font=('arial',23,'bold'),width=40).place(x=10,y=25)
-        Button(self.root,text="SEARCH",height=2,width=25,bg="light blue1").place(x=709,y=25)
+        Button(self.root,text="SEARCH",height=2,width=25,bg="light blue1",command=self.open_file).place(x=709,y=25)
         Button(self.root,text="START",width=97,height=2,bg="light green").place(x=8,y=77)
         Button(self.root,text="SAVE",height=2,width=25,bg="light blue1").place(x=709,y=77)
         Label(self.root,text="FRAME RATE:",bg="lavender").place(x=709,y=150)
-        self.frLabel = Label(self.root,bg='black',width=14)
+        self.frLabel = Label(self.root,bg='black',width=14,fg="red")
         self.frLabel.place(x=790,y=150)
         Label(self.root,text="N FRAMES:",bg="lavender").place(x=721,y=190)
-        self.nframesLabel = Label(self.root,bg='black',width=14)
+        self.nframesLabel = Label(self.root,bg='black',width=14,fg="red")
         self.nframesLabel.place(x=790,y=190)
 
         self.root.mainloop()
-            
+
+    def open_file(self):
+        self.file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
+                        filetypes=(("mp4 files","*.mp4"),("avi files","*.avi"),("all files","*.*")))
+        if self.file != "":
+            probe = ffmpeg.probe(self.file)
+            self.video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
+            self.fr = (self.video_streams[0]['avg_frame_rate'])
+            self.nframes = (self.video_streams[0]['nb_frames'])
+            self.frLabel.configure(text=self.fr)
+            self.nframesLabel.configure(text=self.nframes)
             
         
 if __name__=="__main__":
