67643106c5b959ab4929a03c7d63c924340d25ad
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-04-02 17:09:45 +0200

    Update image_histogram.py

--- image_histogram.py
+++ image_histogram.py
@@ -11,14 +11,17 @@ class Histogram:
     def __init__(self):
         self.window = Tk()
         self.window.title("Image Histogramas")
-        self.window.geometry("719x280")
+        self.window.geometry("717x280")
         
         currentDir = StringVar()
         currentDir.set(os.getcwd())
         self.image_name = StringVar()
 
         Entry(self.window,textvariable=currentDir,width=119).place(x=0,y=0)
-        Entry(self.window,textvariable=self.image_name,width=37,font=("arial",20)).place(x=10,y=40)
+        Entry(self.window,textvariable=self.image_name,width=32,font=("arial",23)).place(x=10,y=40)
+        Button(self.window,text="SEARCH IMAGE",height=2,width=18).place(x=570,y=40)
+        Button(self.window,text="PLOT HISTOGRAM",height=2,width=98).place(x=10,y=100)
+        Button(self.window,text="PLOT CHANNELS HISTOGRAM",height=2,width=98).place(x=10,y=153)
 
         self.window.mainloop()
 
