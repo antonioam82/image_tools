4b47cb034b6764dfb083028717368601111baf94
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-04-02 16:51:05 +0200

    Update image_histogram.py

--- image_histogram.py
+++ image_histogram.py
@@ -3,6 +3,7 @@
 from tkinter import *
 from tkinter import messagebox, filedialog
 import cv2
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,7 +11,14 @@ class Histogram:
     def __init__(self):
         self.window = Tk()
         self.window.title("Image Histogramas")
-        self.window.geometry("623x575")
+        self.window.geometry("719x280")
+        
+        currentDir = StringVar()
+        currentDir.set(os.getcwd())
+        self.image_name = StringVar()
+
+        Entry(self.window,textvariable=currentDir,width=119).place(x=0,y=0)
+        Entry(self.window,textvariable=self.image_name,width=37,font=("arial",20)).place(x=10,y=40)
 
         self.window.mainloop()
 
