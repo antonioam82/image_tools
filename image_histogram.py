580ece4a87c9b168db430b9b8616c129844c6b14
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-04-03 16:50:05 +0200

    Update image_histogram.py

--- image_histogram.py
+++ image_histogram.py
@@ -4,6 +4,7 @@ from tkinter import *
 from tkinter import messagebox, filedialog
 import cv2
 import os
+import threading
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,7 +12,7 @@ class Histogram:
     def __init__(self):
         self.window = Tk()
         self.window.title("Image Histogramas")
-        self.window.geometry("717x280")
+        self.window.geometry("717x167")
         
         currentDir = StringVar()
         currentDir.set(os.getcwd())
@@ -21,8 +22,9 @@ class Histogram:
         Entry(self.window,textvariable=currentDir,width=119).place(x=0,y=0)
         Entry(self.window,textvariable=self.image_name,width=32,font=("arial",23)).place(x=10,y=40)
         Button(self.window,text="SEARCH IMAGE",height=2,width=18,bg="gray80",command=self.open_file).place(x=570,y=40)
-        Button(self.window,text="PLOT HISTOGRAM",height=2,width=98,bg="gray87").place(x=10,y=100)
-        Button(self.window,text="PLOT CHANNELS HISTOGRAM",height=2,width=98,bg="gray87",command=self.channels_hist).place(x=10,y=153)
+        Button(self.window,text="SHOW IMAGE",height=2,width=18,bg="gray80",command=self.show_image).place(x=570,y=100)
+        Button(self.window,text="PLOT HISTOGRAM",height=2,width=37,bg="gray87").place(x=10,y=100)
+        Button(self.window,text="PLOT CHANNELS HISTOGRAM",height=2,width=37,bg="gray87",command=self.channels_hist).place(x=289,y=100)
 
         self.window.mainloop()
 
@@ -31,6 +33,13 @@ class Histogram:
                         filetypes=(("jpg files","*.jpg"),("png files","*.png")))
         if self.image:
             self.image_name.set(self.image)
+
+    def show_image(self):
+        if self.image != "":
+            image = cv2.cvtColor(cv2.imread(self.image), cv2.COLOR_RGB2BGR)
+            plt.figure(figsize=(10,10))
+            plt.imshow(image)
+            plt.show()
 
     def channels_hist(self):
         colors = ('blue', 'green', 'red')
@@ -44,7 +53,7 @@ class Histogram:
             plt.legend()
             plt.title("Histogram Channels")
             plt.grid()
-            plt.show()  
+            plt.show()
 
 if __name__=="__main__":
     Histogram()
