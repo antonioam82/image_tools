1d01d2651e1e7d19cb2990549b30623143034e43
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-12-01 17:29:32 +0100

    Update mkgif.py

--- mkgif.py
+++ mkgif.py
@@ -10,17 +10,45 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-src',type=str,help='Archivo original')
-    parser.add_argument('-dest',type=str,help='Archivo destino')
+    parser.add_argument('-dest',default=os.getcwd(),type=str,help='Archivo destino')
+    parser.add_argument('--start',default=0.0,type=float,help='inicio')
+    parser.add_argument('--end',default=None,type=str,help='final')
     parser.add_argument('--show',default=False,type=bool,help='Mostrar resultado')
     parser.add_argument('-sz',default=100,type=int,help='Tamaño en %')
 
     args=parser.parse_args()
     gm(args)
 
+def show(f):
+    animation = pyglet.image.load_animation(f)
+    bin = pyglet.image.atlas.TextureBin()
+    animation.add_to_texture_bin(bin)
+    sprite = pyglet.sprite.Sprite(animation)
+    w = sprite.width
+    h = sprite.height
+    window = pyglet.window.Window(width=w, height=h)
+
+    @window.event
+    def on_draw():
+        sprite.draw()
+    pyglet.app.run()
+
 def gm(args):
+            
     probe = ffmpeg.probe(args.src)
     video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
-    print("DURATION: ",video_streams[0]['duration'])
+    if args.end:
+        duration = float(args.end)
+    else:
+        duration = video_streams[0]['duration']
+    print("GIF DURATION: ",duration)
+    clip = (VideoFileClip(args.src)
+    .subclip((0,0),(0,float(duration)))
+    .resize(args.sz/100))
+    print('CREATING GIF...')
+    clip.write_gif(args.dest)
+    if args.show == True:
+        show(args.dest)
 
 if __name__=='__main__':
     main()
