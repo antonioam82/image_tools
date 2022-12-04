38e096cef4e44c6473dbbb5fdfd465ba783c69a1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-12-04 16:51:35 +0100

    Update mkgif.py

--- mkgif.py
+++ mkgif.py
@@ -11,8 +11,8 @@ def main():
     parser = argparse.ArgumentParser(description="Create gifs from videos.")
     parser.add_argument('-src','--source',required=True,type=str,help='Ruta archivo original')
     parser.add_argument('-dest','--destination',default='my_gif.gif',type=str,help='Ruta archivo destino')
-    parser.add_argument('-st','--start',default=0.0,type=float,help='Minuto inicial del gif')
-    parser.add_argument('-e','--end',default=None,type=str,help='Minuto final del gif')
+    parser.add_argument('-st','--start',default=0.0,type=float,help='Segundo inicial del gif')
+    parser.add_argument('-e','--end',default=None,type=str,help='Segundo final del gif')
     parser.add_argument('-shw','--show',help='Mostrar resultado',action='store_true')
     parser.add_argument('-sz','--size',default=100,type=int,help='Tamaño en porcentage')
 
@@ -54,3 +54,4 @@ def gm(args):
 
 if __name__=='__main__':
     main()
+
