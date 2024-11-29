643a30357aee2f411018fda38f5b113cde60bd31
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-11-29 17:32:09 +0100

    Update fex.py

--- fex.py
+++ fex.py
@@ -19,35 +19,50 @@ def on_press(key):
 def extract_frames(name,ex,args):
     cam = cv2.VideoCapture(name+ex)
     num_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(num_frames)
     
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    initial_frame = args.from_frame
-    cam.set(cv2.CAP_PROP_POS_FRAMES,initial_frame)###########################
-    total_frames = num_frames - initial_frame###########################
-    print(f"EXTRACTING {total_frames} FRAMES (press space bar to cancel)")
-    pbar = tqdm(total=total_frames,unit='frames',ncols=100)
+
     
-    count = 0
-    ret = True
-    while ret:
-        ret,frame = cam.read()
-        count+=1
+    initial_frame = args.from_frame
+    if args.to_frame:
+        final_frame = args.to_frame
+    else:
+        final_frame = num_frames
+    
+    if (initial_frame >= 0 and initial_frame <= num_frames)and (final_frame > 0 and final_frame <= num_frames):
+        cam.set(cv2.CAP_PROP_POS_FRAMES,initial_frame)###########################
+        total_frames = abs(num_frames - initial_frame) - abs(final_frame - num_frames)###########################
+        print(f"EXTRACTING {total_frames} FRAMES (press space bar to cancel)")
+        pbar = tqdm(total=total_frames,unit='frames',ncols=100)
+    
+        count = 0
+        ret = True
+        while ret:
+            ret,frame = cam.read()
+            count+=1
         
-        if ret:
-            cv2.imwrite(name+str(count)+".png",frame)
-            pbar.update(1)
+            if ret:
+                cv2.imwrite(name+str(count)+".png",frame)
+                pbar.update(1)
+
+            current_frame = int(cam.get(cv2.CAP_PROP_POS_FRAMES))
+            if current_frame >= args.to_frame:
+                break
             
-        if stop:
+            if stop:
                 print(Fore.YELLOW + Style.NORMAL + "\nFrame extraction interrupted by user." + Fore.RESET + Style.RESET_ALL)
                 pbar.disable = True
                 break
                 
-    cam.release()
-    pbar.close()
-    listener.stop()
-    if not stop:
-        print("DONE")
+        cam.release()
+        pbar.close()
+        listener.stop()
+        if not stop:
+            print("DONE")
+    else:
+        print(Fore.RED+Style.BRIGHT+"Invalid index for initial or final frame."+Fore.RESET+Style.RESET_ALL)
     
 def check_source_ext(file):
     supported_formats = ['.mp4','.avi','.mov','.wmv','.rm','.webp']
@@ -65,6 +80,7 @@ def main():
                                      epilog = "...")
     parser.add_argument('-src','--source',required=True,type=check_source_ext,help='Source file name')
     parser.add_argument('-from','--from_frame',default=0,type=int,help='Frame index to extract from')
+    parser.add_argument('-to','--to_frame',default=None,type=int,help='Frame index to extract to')
 
     args = parser.parse_args()
     name, extension = os.path.splitext(args.source)
